39a8dce528f7aca268a1d4b5f54b7bb2da5cfa20
Beatrix Haddock <beatrixh@gen-uge-archive-p008.cluster.ihme.washington.edu>
2021-02-27 12:49:49 -0800

    trying to update version for added hobbs file

--- src/vivarium_helpers/__about__.py
+++ src/vivarium_helpers/__about__.py
@@ -7,7 +7,7 @@ __title__ = "vivarium_helpers"
 __summary__ = "Tools maintained by the research side of the simluation science team."
 __uri__ = "https://github.com/ihmeuw/vivarium_helpers"
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __author__ = "Beatrix Haddock"
 __email__ = "beatrixh@uw.com"
