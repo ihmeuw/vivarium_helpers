c0e540c66ed47e70fe9e807c1de06cdd09e5ae21
Beatrix Haddock <beatrixh@gen-uge-archive-p008.cluster.ihme.washington.edu>
2021-02-27 19:16:03 -0800

    updating ver num

--- src/vivarium_helpers/__about__.py
+++ src/vivarium_helpers/__about__.py
@@ -7,7 +7,7 @@ __title__ = "vivarium_helpers"
 __summary__ = "Tools maintained by the research side of the simluation science team."
 __uri__ = "https://github.com/ihmeuw/vivarium_helpers"
 
-__version__ = "0.0.1"
+__version__ = "0.0.11"
 
 __author__ = "Beatrix Haddock"
 __email__ = "beatrixh@uw.com"
