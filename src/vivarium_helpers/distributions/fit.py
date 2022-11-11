ae52e29e297d4b07aad3c86f54e6173b89d4de12
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-11-11 14:07:30 -0800

    add first version of method_of_moments function from method of moments notebook

--- src/vivarium_helpers/distributions/fit.py
+++ src/vivarium_helpers/distributions/fit.py
@@ -15,3 +15,26 @@ quadratic_loss = l2_loss
 def weighted_l2_loss(x,y,weights):
     x,y,weights = map(np.asarray, [x,y,weights])
     return ((weights*(x-y))**2).sum()
+
+def method_of_moments(
+    mean,
+    variance,
+    distribution,
+    initial_parameters,
+    loss = quadratic_loss,
+    **kwargs,
+):
+    # args stores extra (positional) arguments to pass to distribution(),
+    # as defined in documentation for `minimize`
+    if 'args' not in kwargs:
+        print('No args')
+        kwargs['args'] = ()
+
+    def objective_function(parameters, *args):
+        dist = distribution(*parameters, *args)
+        mv = dist.stats()
+        return loss(mv, [mean,variance])
+
+    result = minimize(objective_function, initial_parameters, **kwargs)
+    best_params = result.x
+    return distribution(*best_params, *kwargs['args']), result
