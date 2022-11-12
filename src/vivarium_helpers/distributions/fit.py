d7bbe9271ac287f26c31c3e4c5a175cfd9b6209e
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-11-12 12:57:25 -0800

    use elif in arglist function; handle args keyword completely in method_of_moments instead of passing it to minimize

--- src/vivarium_helpers/distributions/fit.py
+++ src/vivarium_helpers/distributions/fit.py
@@ -23,7 +23,7 @@ def _parse_arglist(arg_list):
     # Conform arg_list to the form [*args, kwargs]
     if isinstance(arg_list, dict):
         arg_list = [arg_list]
-    if len(arg_list) == 0 or not isinstance(arg_list[-1], dict):
+    elif len(arg_list) == 0 or not isinstance(arg_list[-1], dict):
         arg_list = [*arg_list, {}]
     *args, kwargs = arg_list
     return args, kwargs
@@ -38,11 +38,12 @@ def method_of_moments(
     mean, variance = moments
     # args stores extra (positional) arguments to pass to distribution(),
     # as defined in documentation for `minimize`
-    if 'args' not in kwargs:
-        print('No args')
-        kwargs['args'] = ()
+    # if 'args' not in kwargs:
+    #     print('No args')
+    #     kwargs['args'] = ()
 
-    fixed_args, fixed_kwargs = _parse_arglist(kwargs['args'])
+    fixed_arglist = kwargs.pop('args', ())
+    fixed_args, fixed_kwargs = _parse_arglist(fixed_arglist)
 
     # Ensure that initial_parameters has the form [*args, kwargs]
     # if isinstance(initial_parameters, dict):
@@ -67,19 +68,19 @@ def method_of_moments(
     initial_parameters = [*pos_params, *kwd_params.values()]
 
     print(initial_parameters)
-    def dist_from_parameters(parameters, *args):
+    def dist_from_parameters(parameters):
         pos_params = parameters[:num_positional]
         kwd_params = dict(zip(param_keys, parameters[num_positional:]))
-        assert (fixed_args, fixed_kwargs) == _parse_arglist(args), \
-            f"{fixed_args=}, {fixed_kwargs=}, {args=}"
+        # assert (fixed_args, fixed_kwargs) == _parse_arglist(args), \
+        #     f"{fixed_args=}, {fixed_kwargs=}, {args=}"
         kwd_args = {**kwd_params, **fixed_kwargs}
         return distribution(*pos_params, *fixed_args, **kwd_args)
 
-    def objective_function(parameters, *args):
-        dist = dist_from_parameters(parameters, *args)
+    def objective_function(parameters):
+        dist = dist_from_parameters(parameters)
         mv = dist.stats()
         return loss(mv, [mean,variance])
 
     result = minimize(objective_function, initial_parameters, **kwargs)
     best_params = result.x
-    return dist_from_parameters(best_params, *kwargs['args']), result
+    return dist_from_parameters(best_params), result
