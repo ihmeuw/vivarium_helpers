06c90b6f43d8421053bcad8277dc40ce5a710ffe
Nathaniel Blair-Stahn <ndbs@uw.edu>
2022-11-12 00:18:12 -0800

    add ability to pass keyword arguments as fixed parameters when fitting a distribution

--- src/vivarium_helpers/distributions/fit.py
+++ src/vivarium_helpers/distributions/fit.py
@@ -19,6 +19,15 @@ def weighted_l2_loss(x,y,weights):
 def arglist(*args, **kwargs):
     return [*args, kwargs]
 
+def _parse_arglist(arg_list):
+    # Conform arg_list to the form [*args, kwargs]
+    if isinstance(arg_list, dict):
+        arg_list = [arg_list]
+    if len(arg_list) == 0 or not isinstance(arg_list[-1], dict):
+        arg_list = [*arg_list, {}]
+    *args, kwargs = arg_list
+    return args, kwargs
+
 def method_of_moments(
     moments,
     distribution,
@@ -26,22 +35,30 @@ def method_of_moments(
     loss = quadratic_loss,
     **kwargs,
 ):
+    mean, variance = moments
     # args stores extra (positional) arguments to pass to distribution(),
     # as defined in documentation for `minimize`
     if 'args' not in kwargs:
         print('No args')
         kwargs['args'] = ()
 
-    mean, variance = moments
-    # Ensure that initial_parameters has the form [*args, kwargs]
-    if isinstance(initial_parameters, dict):
-        initial_parameters = [initial_parameters]
-    print(initial_parameters)
-    if not isinstance(initial_parameters[-1], dict):
-        initial_parameters = [*initial_parameters, {}]
+    fixed_args, fixed_kwargs = _parse_arglist(kwargs['args'])
 
-    # Extract positional and keyword parameters from parameter list
-    *pos_params, kwd_params = initial_parameters
+    # Ensure that initial_parameters has the form [*args, kwargs]
+    # if isinstance(initial_parameters, dict):
+    #     initial_parameters = [initial_parameters]
+    # print(initial_parameters)
+    # if not isinstance(initial_parameters[-1], dict):
+    #     initial_parameters = [*initial_parameters, {}]
+    #
+    # # Extract positional and keyword parameters from parameter list
+    # *pos_params, kwd_params = initial_parameters
+    pos_params, kwd_params = _parse_arglist(initial_parameters)
+    print(fixed_kwargs, kwd_params)
+    if not fixed_kwargs.keys().isdisjoint(kwd_params.keys()):
+        raise ValueError(
+            "Optimization keyword parameters overlap with fixed"
+            f" keyword parameters: {fixed_kwargs.keys() & kwd_params.keys()}")
     # Record # of positional arguments for convenience in objective_function
     num_positional = len(pos_params)
     # Save keys for keyword parameters to pass to distribution in objective_function.
@@ -53,7 +70,10 @@ def method_of_moments(
     def dist_from_parameters(parameters, *args):
         pos_params = parameters[:num_positional]
         kwd_params = dict(zip(param_keys, parameters[num_positional:]))
-        return distribution(*pos_params, *args, **kwd_params)
+        assert (fixed_args, fixed_kwargs) == _parse_arglist(args), \
+            f"{fixed_args=}, {fixed_kwargs=}, {args=}"
+        kwd_args = {**kwd_params, **fixed_kwargs}
+        return distribution(*pos_params, *fixed_args, **kwd_args)
 
     def objective_function(parameters, *args):
         dist = dist_from_parameters(parameters, *args)
