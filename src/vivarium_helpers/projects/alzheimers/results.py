efb566da700d22493f9a645a561a937750618c02
Nathaniel Blair-Stahn <ndbs@uw.edu>
2025-11-18 16:17:28 -0800

    add summarize_and_beautify and related methods

--- src/vivarium_helpers/projects/alzheimers/results.py
+++ src/vivarium_helpers/projects/alzheimers/results.py
@@ -1,4 +1,5 @@
-from ...utils import AttributeMapping
+from ...utils import (AttributeMapping, constant_categorical, current_time,
+                      convert_to_categorical)
 from ...vph_output.measures import VPHResults
 from ...vph_output.operations import VPHOperator
 from ...vph_output.loading import load_draws_from_keyspace_files, load_keyspace
@@ -165,3 +166,116 @@ class AlzheimersResultsProcessor:
             # name if None was passed
             self.data[save_name or measure] = sim_output
 
+    def scale_to_real_world(self, measure):
+        """Divide the values in the `measure` dataframe by the values in
+        `model_scale`, matching location and draw, and broadcasting across
+        other columns in `measure`. This computes the value of the measure
+        in the real-world population from the scaled-down version we get
+        from the simulation.
+        """
+        measure = self.ops.value(measure)
+        # NOTE: Reindexing preserves categoricals (in location column), but
+        # results in all NaN's for some reason
+        model_scale = self.ops.value(self.model_scale)#.reindex(measure.index)
+        # scaled_measure = measure.divide(model_scale, axis=0).reset_index()
+        scaled_measure = (measure / model_scale).reset_index()
+        #.dropna() # Alternative to filtering draws above
+        return scaled_measure
+
+    def calculate_rate(
+            self,
+            measure,
+            # Default stratifications: ['event_year', 'age_group', 'sex']
+            stratifications=None,
+            append=False,
+        ):
+        """Divide a measure by person-time to get a rate."""
+        if stratifications is None:
+            stratifications = ['event_year', 'age_group', 'sex']
+        rate = self.ops.drop_index('scenario').ratio(
+            measure,
+            self.person_time,
+            stratifications,
+            numerator_broadcast='scenario',
+            record_inputs=False,
+        )
+        if append:
+            # Concatenate original measure DataFrame with rates, adding a
+            # 'metric' column to distinguish between them
+            metric_dtype = pd.CategoricalDtype(['Number', 'Rate'])
+            measure = measure.assign(
+                metric=constant_categorical(
+                    'Number', len(measure), metric_dtype))
+            rate = rate.assign(
+                metric=constant_categorical('Rate', len(rate), metric_dtype))
+            result = pd.concat([measure, rate], ignore_index=True)
+        else:
+            # Just return the calculated rates
+            result = rate
+        return result
+
+    def summarize_and_beautify(
+            self,
+            df,
+            # By default, assume disease stage is stored in sub_entity
+            # column, but allow passing a different column
+            disease_stage_column='sub_entity',
+        ):
+        """Append rates, scale to real-world, summarize, rename columns,
+        filter to desired columns, and put them in the right order.
+        """
+        # Default column name map
+        column_name_map = {
+            'event_year': 'Year',
+            'age_group': 'Age',
+            'location': 'Location',
+            'sex': 'Sex',
+            'scenario': 'Scenario',
+            'measure': 'Measure',
+            'metric': 'Metric',
+            disease_stage_column: 'Disease Stage',
+            'mean': 'Mean',
+            'lower': '95% UI Lower',
+            'upper': '95% UI Upper',
+        }
+        disease_stage_name_map = {
+            'alzheimers_blood_based_biomarker_state': 'Preclinical AD',
+            'alzheimers_mild_cognitive_impairment_state': 'MCI due to AD',
+            'alzheimers_disease_state' : 'AD Dementia'
+        }
+        scenario_name_map = {
+            'baseline': 'Reference',
+            'bbbm_testing': 'BBBM Testing Only',
+            'bbbm_testing_and_treatment' : 'BBBM Testing and Treatment',
+        }
+        column_order = [
+            'Year', 'Location', 'Age', 'Sex' , 'Disease Stage' , 'Scenario',
+            'Measure', 'Metric', 'Mean', '95% UI Lower', '95% UI Upper',
+        ]
+        current_time()
+        # Do transformations
+        df = (
+            df
+            # Filter out burn-in years before 2025
+            .query('event_year >= 2025')
+            # Scale to real-world values
+            .pipe(self.scale_to_real_world)
+            # # Append rows for "all ages" and "both sexes"
+            # .pipe(append_aggregate_categories, ops)
+            # Calculate and append rates
+            .pipe(self.calculate_rate, append=True)
+            # Compress data if possible
+            .pipe(convert_to_categorical)
+            .pipe(lambda df: current_time() or df)
+            # Summarize data
+            .pipe(self.ops.summarize_draws)
+            .reset_index()
+            .pipe(lambda df: current_time() or df)
+            # Rename columns
+            .rename(columns=column_name_map)
+            .replace(
+                {'Disease Stage': disease_stage_name_map,
+                'Scenario': scenario_name_map})
+            [column_order]
+        )
+        return df
