ff7658d77919895a339177f9031b7444a7c205b4
Steve Bachmeier <sbachmei@uw.edu>
2026-01-15 09:50:16 -0700

    create some dumb thing

---
+++ src/vivarium_helpers/useless_thing.py
@@ -0,0 +1,19 @@
+from vivarium_helpers.lbwsg import demography 
+
+import numpy as np
+import pandas as pd
+
+
+class UselessThing:
+
+
+    def define_then_do_not_use_dataframe(self):
+
+
+        df = pd.DataFrame({"column_1": range(10), "column_2":range(10), "column_3":range(10), "column_4":range(10)})
+
+
+    
+
+
+
